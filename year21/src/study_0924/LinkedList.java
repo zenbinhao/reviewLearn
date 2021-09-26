1eb4908324af1203d52058bc9055d7915423b3f1
binhao.zeng <594192936@qq.com>
2021-09-26 16:10:25 +0800

    数据结构的学习

--- year21/src/study_0924/LinkedList.java
+++ year21/src/study_0924/LinkedList.java
@@ -1,4 +1,10 @@
 package study_0924;
+//单链表
+//java.util.LinkedList  单链表
+
+//import java.util.Queue; 队列
+
+import java.util.Stack;
 
 public class LinkedList {
     public static void main(String[] args) {
@@ -23,9 +29,30 @@ public class LinkedList {
 
         linkList.list();
 
+        linkList.update(new PersonNode(1,"曾斌昊"));
+
+        linkList.list();
+
+//        linkList.delete(1);
+//        linkList.delete(2);
+//        linkList.delete(3);
+//        linkList.delete(4);
+//
+//        linkList.list();
+
+        System.out.println("单链表的翻转");
+        LinkList linkList1 = new LinkList();
+        linkList1.setHead(linkList.reverseAdd());
+        linkList1.list();
+
+        System.out.println("\n\n\n使用String 字符串拼接逆序打印");
+        linkList1.reversePr();
+        System.out.println("\n\n\n使用数据结构 栈进行逆序打印");
+        linkList1.reversePr1();
 
     }
 }
+//个人类
 class PersonNode{
     int no;
     String name;
@@ -45,8 +72,18 @@ class PersonNode{
                 '}';
     }
 }
+
+//带头单链表管理类
 class LinkList{
     private PersonNode head = new PersonNode(0,"");
+
+    public PersonNode getHead() {
+        return head;
+    }
+
+    public void setHead(PersonNode head) {
+        this.head = head;
+    }
 
     //添加节点函数
     public void add(PersonNode person){
@@ -94,10 +131,87 @@ class LinkList{
         temp.next = person;
 //        temp.next = person;
     }
+
+    //根据编号修改函数
+    public void update(PersonNode newPersonNode){
+        PersonNode temp = head;
+        //标志位 判断是否找到修改的编号  因为存在无匹配编号的情况存在
+        boolean flag = false;
+        while (true){
+            if(temp.no == newPersonNode.no){
+                //找到相同编号则修改标志位
+                flag =true;
+                //并退出遍历查找
+                break;
+            }
+            //指针的下一个地址为空
+            if(temp.next==null){
+                break;
+            }
+            // 通过循环 指针后移进行单链表遍历
+            temp = temp.next;
+        }
+        //找到对应内存地址后 进行修改值
+        if (flag){
+            temp.name = newPersonNode.name;
+            System.out.println("成功修改");
+        }else {
+            System.out.printf("没找到编号为的%d数据信息，无法修改",newPersonNode.no);
+        }
+
+    }
+
+    public void delete(int no){
+        // 辅助指针
+        PersonNode temp = head;
+        // 需要一个标志位看是否找到对应需要删除的 编号才可以进行删除操作
+        boolean flag = false;
+        //通过while循环寻找 需要删除的节点的前一个节点
+        while (true){
+            //找到对应节点的前一个节点
+            if(temp.next.no==no){
+                flag =true;
+                break;
+            }
+            if(temp.next==null){
+                break;
+            }
+            temp = temp.next;
+        }
+        //存在对应节点 进行删除 原来被覆盖的引用的temp.next对象 将会被gc自动回收
+        if(flag){
+            // 对应节点的引用进行覆盖
+            temp.next = temp.next.next;
+            System.out.println("成功删除");
+        }else {
+            System.out.println("未找到可以删除的对应编号");
+        }
+    }
+
+    public PersonNode reverseAdd(){
+        //新头
+        PersonNode newHead = new PersonNode(0,"");
+        PersonNode temp = head.next;
+        while(temp!=null){
+            //存下当前那个
+            PersonNode a = temp;
+            //移动到下一个
+            temp = temp.next;
+
+            //头插法
+            a.next = newHead.next;
+            newHead.next = a;
+        }
+        return newHead;
+    }
+
     //显示函数
     public void list(){
         PersonNode temp = head.next;
-
+        if(head.next==null){
+            System.out.println("链表为空");
+            return;
+        }
         //通过while循环找到链表的最后位置
         while (true){
             //如果 temp.next对象的内存地址为空 则结束循环  说明temp为链表的最后一个节点
@@ -108,6 +222,33 @@ class LinkList{
             System.out.println(temp);
             //如果不是空则继续找到链表的末尾
             temp = temp.next;
+        }
+    }
+
+    //逆序打印
+    public void reversePr(){
+        //辅助指针
+        PersonNode temp = head.next;
+        String sum = "";
+        while (temp!=null){
+            sum = temp.toString()+"\n"+ sum;
+            temp = temp.next;
+        }
+        System.out.println(sum);
+    }
+    public void reversePr1(){
+        //栈对象
+        Stack<PersonNode> personNodes = new Stack<>();
+        //辅助指针
+        PersonNode temp = head.next;
+        while (temp!=null){
+            //入栈  先入后出
+            personNodes.add(temp);
+            temp = temp.next;
+        }
+        while (!personNodes.isEmpty()){
+            //出栈
+            System.out.println(personNodes.pop().toString());
         }
     }
 }
